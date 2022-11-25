91821d0d575ba1a96e49b58fd94bc67fbe252d03
Christopher Ashton <christopher.ashton@madetech.com>
2022-11-25 18:22:22 +0000

    Added first test to ensure it works as intended

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -4,6 +4,11 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
+  it("should return an item with -1 less sellIn", function() {
+    const gildedRose = new Shop([new Item("grapes", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+  })
 });
