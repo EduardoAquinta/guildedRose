29f2ab4be4c8a694f17a8a249e474a2dadef057f
eddie ashton <eddie.ashton@madetech.com>
2023-03-10 16:57:32 +0000

    unnested all the if statements, add comments

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -18,61 +18,65 @@ class Shop {
       }
 
       //applies -1 quality if not brie or BSP if quality is above 0
-      if (this.items[i].name !== 'Aged Brie' && this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
+      if (this.items[i].name !== 'Aged Brie'
+          && this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert'
+          && this.items[i].quality > 0)
+      {
             this.items[i].quality -= 1;
-          }
-      } else {
+      }
+
+      //Aged Brie and BSP increase in quality each day
+      if(this.items[i].name === "Aged Brie" || this.items[i].name === "Backstage passes to a TAFKAL80ETC concert")
+      {
+        this.items[i].quality += 1;
+      }
+
         //BSP increases in quality the closer to the sellIn date.
-        if (this.items[i].quality < 50) {
-          this.items[i].quality += 1;
-          if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
-                //increase in quality by 1 per day until 6 days left
-                if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-                    this.items[i].quality += 1;
-                }//increase in quality by 2 per day between 5 and  0 days left
-                if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-                  this.items[i].quality += 1;
-                }
-          }
-        }
-      }
-      //if sellin less than 0 and quality is greater than 0 decrease quality by 1, unless brie, BSP or Sulfuras
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name !== 'Aged Brie') {
-          if (this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality -= 1;
-              }
-            }
-          } else {
-            //quality stays at zero once it reaches 0
-            this.items[i].quality = 0;
-          }
-        } else {//increase quality by 1 until 50 is reached for Brie and BSP
-          if (this.items[i].quality < 50) {
+      if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert")
+      {
+        switch (true)
+        {
+          case (this.items[i].sellIn > 5 && this.items[i].sellIn <= 10) :
             this.items[i].quality += 1;
-          }
+            break;
+          case this.items[i].sellIn <= 5:
+            this.items[i].quality += 2;
         }
       }
 
-      //Sulfuras can have its own quality assignment
-      if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].quality = 80;
-      }
-
-      //Conjured has an extra -1 quality.
-      if (this.items[i].name.includes("Conjured")){
+      //when the item is normal and has passed it's sellIn date, quality degrades an extra step
+      if(this.items[i].sellIn < 0 && this.items.name !== "Aged Brie"
+          && this.items.name !== "Backstage passes to a TAFKAL80ETC concert")
+      {
         this.items[i].quality -= 1;
       }
 
+      //Backstage pass is worthless after the concert, as is any item that has reached quality zero
+      if( (this.items[i].sellIn < 0
+              && this.items[i].name === "Backstage passes to a TAFKAL80ETC concert")
+                || this.items[i].quality <= 0) {
+        this.items[i].quality = 0;
+      }
+      //Item quality can never reach above 50
+      if( this.items[i].quality >= 50)
+      {
+        this.items[i].quality = 50;
+      }
 
+      //Sulfuras can have its own quality assignment
+      if (this.items[i].name === 'Sulfuras, Hand of Ragnaros')
+      {
+        this.items[i].quality = 80;
+      }
+      //Conjured has an extra -1 quality.
+      if (this.items[i].name.includes("Conjured"))
+      {
+        this.items[i].quality -= 1;
+      }
     }
     return this.items;
   }
 }
-
 module.exports = {
   Item,
   Shop
